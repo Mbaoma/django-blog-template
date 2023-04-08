307631192ce5b3cfbed74b185dd0d21669a3a83e
Mbaoma <mmaryraphaella@gmail.com>
2023-04-08 08:37:23 +0100

    post test

--- tutorial/blog/models.py
+++ tutorial/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    '''
+    Model for blog posts which users create
+    '''
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    date_posted = models.DateField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #when a user is deleted, delete the post
+
+    def __str__(self):
+        return self.title
