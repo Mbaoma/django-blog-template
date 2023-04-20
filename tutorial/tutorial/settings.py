a8ea4f006ae113f2dd6975a8334f9e944ca13ddd
Mbaoma <mmaryraphaella@gmail.com>
2023-04-20 17:35:18 +0100

    update and create posts

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -29,6 +29,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog.apps.BlogConfig",
+    "account.apps.AccountConfig",
+    "crispy_forms",
+    "crispy_bootstrap4"
 ]
 
 MIDDLEWARE = [
@@ -113,3 +116,14 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+LOGIN_REDIRECT_URL = "blog-home"
+
+LOGIN_URL = 'login'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -16,8 +16,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.contrib.auth import views as auth_views
+from account import views as account_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("blog.urls")),
+    path("register/", account_views.register, name="register"),
+    path("login/", auth_views.LoginView.as_view(template_name="account/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(template_name="account/logout.html"), name="logout"),
+    path("profile/", account_views.profile, name="profile"),
+    path("", include("blog.urls")),    
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
